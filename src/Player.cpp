126c7106cad06f1ae6a9dd09ea4b6b6b1b3d1c03
Kelyn Alexander B. Gutiérrez <alekelbar@localhost.localdomain>
2023-11-09 21:10:41 -0600

    teletransport

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,10 +1,12 @@
 #include "../include/Player.hpp"
 #include "Player.hpp"
 
-void Player::move() {
+void Player::move()
+{
   sf::Vector2f movement = this->sprite->getPosition();
 
-  switch (this->dir) {
+  switch (this->dir)
+  {
   case Player::up:
     movement.y -= speed;
     break;
@@ -21,21 +23,44 @@ void Player::move() {
     break;
   }
 
+  // Mecanico de teletransportación de por las paredes...
+  if (movement.x < 0)
+  {
+    movement.x = 391;
+  }
+  if (movement.y < 0)
+  {
+    movement.y = 391;
+  }
+
+  if (movement.x > 391)
+  {
+    movement.x = 0;
+  }
+
+  if (movement.y > 391)
+  {
+    movement.y = 0;
+  }
+
   this->sprite->setPosition(movement);
 }
 
-void Player::setDirection(direction dir) {
+void Player::setDirection(direction dir)
+{
   this->sprite->setTexture(textures.at(dir));
   this->dir = dir;
 }
 
-Player::Player() {
+Player::Player()
+{
   // Cargar la textura...
   const std::string filename = "assets/pacman/";
 
   // Llenar el vector textures antes de usarlo
   textures.resize(4);
-  for (size_t i = 0; i < textures.size(); i++) {
+  for (size_t i = 0; i < textures.size(); i++)
+  {
     if (!textures.at(i).loadFromFile(filename + std::to_string(i) + ".png"))
       throw FailedToLoadTexture(filename + std::to_string(i) + ".png");
   }
